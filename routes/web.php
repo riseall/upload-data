240fa53b6231394e7b28757b9bf137cf70f2ce9a
Rizal Nugroho <rizalnug2@gmail.com>
2025-07-16 16:33:00 +0700

    feat: add api root

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UploadController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,11 +17,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('auth/login');
+})->name('home');
+
+Route::get('/login', function () {
+    return view('auth/login');
+})->name('login');
+
+Route::get('/regUser', function () {
+    return view('auth/register');
+})->name('register');
+
+Route::middleware('web')->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/upload', [UploadController::class, 'showUpload'])->name('upload.show');
+    Route::get('/data', [UploadController::class, 'showData'])->name('data.show'); // Nama rute yang lebih jelas
+
+    Route::post('/upload', [UploadController::class, 'uploadData'])->name('upload.data');
 });
-Route::get('/upload', [UploadController::class, 'showUpload'])->name('upload.show');
-Route::post('/upload', [UploadController::class, 'uploadData'])->name('upload.data');
-Route::get('/data', [UploadController::class, 'showData'])->name('upload.show');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
